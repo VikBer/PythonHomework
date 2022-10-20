445cfa462d30eef269f6e692501865bd15c2de55
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-10-20 22:36:26 +0300

    task solved

--- HW.py
+++ HW.py
@@ -1,10 +1,11 @@
 # Семинар 2:
-# git checkout Sem2Task1 - Задача 1 -  Для натурального n создать словарь индекс-значение, 
-#                                       состоящий из элементов последовательности 3n + 1.
+# git checkout Sem2Task2 - Задача 2 - Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.    состоящий из элементов последовательности 3n + 1.
 
 n = int(input("Введите число n \n"))
-dictionary = {}
-for i in range(1,n):
-    dictionary.update({i:3*i+1})
+arr = [1]
+factorial = 1
+for i in range(2, n+1):
+    factorial *= i
+    arr.append(factorial)
 
-print(dictionary)
+print(arr)
