cfeb47bc4582847fa38e159dfaf774183d9b8aa7
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-10-20 17:52:57 +0300

    task solved

--- HW.py
+++ HW.py
@@ -2,3 +2,9 @@
 # git checkout Sem2Task1 - Задача 1 -  Для натурального n создать словарь индекс-значение, 
 #                                       состоящий из элементов последовательности 3n + 1.
 
+n = int(input("Введите число n \n"))
+dictionary = {}
+for i in range(1,n):
+    dictionary.update({i:3*i+1})
+
+print(dictionary)
