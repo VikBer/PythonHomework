59dff3c98947678af2c53f90a9ba3147cbfac53a
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-10-20 23:47:04 +0300

    task solved

--- HW.py
+++ HW.py
@@ -2,6 +2,13 @@
 # git checkout Sem2Task4 - Задача 4 - Задайте число N и создайте список, заполненный числами из промежутка [-N, N]. 
 # Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 
+
+
+
 n = int(input("Введите число n \n"))
-lst = [round((1+1/i)**i, 2) for i in range(1, n+1)]
-print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 2)}')
+lst = [i for i in range(-n, n)]
+print(lst)
+
+file = open('file.txt', 'r')
+res = lst[int(file.readline())]*lst[int(file.readline(2))]*lst[int(file.readline(3))]*lst[int(file.readline(4))]
+print(res)
