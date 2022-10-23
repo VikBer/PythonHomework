cfe1ae38b29af136c2446d02029364dd6e9ecb10
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-10-23 23:43:47 +0300

    task solved

--- HW.py
+++ HW.py
@@ -1,10 +1,32 @@
 # Семинар 4:
-# git checkout Sem4Task3 - Задача 3 - Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 
-a = [1,21,3,4,5,4,3,2,21,10]
+from random import randint
 
-lst = []
-for i in a:
-    if i not in lst:
-        lst.append(i)
-print(lst)
+k = int(input('Введите степень k:'))
+
+fact = []
+for i in range (1, k +2):
+    fact.append(randint(1,101))
+
+itog = []
+for i in range(len(fact)):
+    if k == 1:
+        itog.append(f'{fact[i]}*x')
+    elif k == 0:
+        itog.append(f'{fact[i]}')
+    else:
+        itog.append(f'{fact[i]}*x^{k}')
+    signs = randint(0,1)
+    if signs == 1:
+        itog.append('+')
+    elif signs == 0:
+        itog.append('-')
+    k -= 1
+
+itog.pop(-1) 
+itog.append('=0')
+
+record = open('task4.txt', 'w')
+record.write(''.join(itog))
+record.close()
